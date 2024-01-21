81221850d0d94058b7f6c8fd4a2155618783f1c3
Amiya Maity <amiyamaity7105@gmail.com>
2024-01-21 16:08:41 +0530

    build(deps): update 3 files and delete 1 file

--- src/extension.ts
+++ src/extension.ts
@@ -1,71 +1,118 @@
 'use strict';
 import * as path from 'path';
 import * as vscode from 'vscode';
-import * as child_process from 'child_process';
-import { fileURLToPath } from 'url';
+
+interface CustomTerminal {
+    terminal: vscode.Terminal;
+    cwd: string;
+}
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('runcfile.runc', () => {
-		// Get the active text editor
-		const editor = vscode.window.activeTextEditor;
-	
-		if (editor) {
-			let filePath = editor.document.fileName;
-			filePath = filePath.replaceAll("\\", "/");
-			
-			if (filePath.endsWith('.c') && filePath.charAt(1) == ':') {
-				filePath = "/mnt/"+filePath;
-				filePath = filePath.replaceAll(":", "");
-				const fileName = path.basename(filePath);
-				const op = filePath.replace(fileName, "a.out");
-				const opn = filePath.replace(fileName, "");
-				// Create a terminal
-				const terminal = vscode.window.createTerminal({
-					name: 'WSL - Ubuntu',
-					shellPath: 'wsl.exe',
-					env: {
-					},
-					cwd: '',
-				});
-	
-				// Build and run the command
-				const gccCommand = `gcc "${filePath}" -lm -o  "${op}" ; chmod +x "${op}"; PATH=$PATH:"${opn}"; "${op}"; rm "${op}"`;
-				
-				// Send the command to the terminal
-				terminal.sendText(gccCommand, true);
-	
-				// Show the terminal
-				terminal.show();
-			}
-			else if (filePath.endsWith('.c') && filePath.charAt(1) != ':') {
-				filePath = filePath.replaceAll("//wsl.localhost/Ubuntu", ""); 
-				const fileName = path.basename(filePath);
-				const op = filePath.replace(fileName, "a.out");
-				const opn = filePath.replace(fileName, "");
-				// Create a terminal
-				const terminal = vscode.window.createTerminal({
-					name: 'WSL - Ubuntu',
-					shellPath: 'wsl.exe',
-					env: {
-					},
-					cwd: '',
-				});
+    const createdTerminals: CustomTerminal[] = [];
 
-				// Build and run the command
-				const gccCommand = `gcc "${filePath}" -lm -o "${op}" ; chmod +x "${op}"; PATH=$PATH:"${opn}"; "${op}"; rm "${op}"`;
-				
-				// Send the command to the terminal
-				terminal.sendText(gccCommand, true);
-	
-				// Show the terminal
-				terminal.show();
-			} else {
-				vscode.window.showWarningMessage('Not a c file (.c).');
-			}
-		} else {
-			vscode.window.showWarningMessage('Open a c file to run with GCC.');
-		}
-	});
-	
-	context.subscriptions.push(disposable);
+    const disposable = vscode.commands.registerCommand('runcfile.runc', () => {
+        // Get the active text editor
+        const editor = vscode.window.activeTextEditor;
+
+        if (editor) {
+            let filePath = editor.document.fileName;
+            filePath = filePath.replaceAll("\\", "/");
+
+            let customTerminal: CustomTerminal | undefined;
+
+            if (filePath.endsWith('.c') && filePath.charAt(1) == ':') {
+                filePath = "/mnt/" + filePath;
+                filePath = filePath.replaceAll(":", "");
+                const fileName = path.basename(filePath);
+                const op = filePath.replace(fileName, "a.out");
+                const opn = filePath.replace(fileName, "");
+
+                // Check if a terminal for C files already exists
+                customTerminal = createdTerminals.find(term => term.terminal.name === 'WSL - Ubuntu' && term.cwd === opn);
+
+                if (!customTerminal) {
+                    // Create a new terminal
+                    const terminal = vscode.window.createTerminal({
+                        name: 'WSL - Ubuntu',
+                        shellPath: 'wsl.exe',
+                        env: {},
+                    });
+
+                    customTerminal = { terminal, cwd: opn };
+                    createdTerminals.push(customTerminal);
+                }
+
+                // Build and run the command
+                const gccCommand = `gcc "${filePath}" -lm -o  "${op}" && chmod +x "${op}" && "${op}" && rm "${op}"`;
+
+                // Send the command to the terminal
+                customTerminal.terminal.sendText(gccCommand, true);
+
+                // Show the terminal
+                customTerminal.terminal.show();
+            } else if (filePath.endsWith('.c') && filePath.charAt(1) != ':') {
+                filePath = filePath.replaceAll("//wsl.localhost/Ubuntu", "");
+                const fileName = path.basename(filePath);
+                const op = filePath.replace(fileName, "a.out");
+                const opn = filePath.replace(fileName, "");
+
+                // Check if a terminal for C files already exists
+                customTerminal = createdTerminals.find(term => term.terminal.name === 'WSL - Ubuntu' && term.cwd === opn);
+
+                if (!customTerminal) {
+                    // Create a new terminal
+                    const terminal = vscode.window.createTerminal({
+                        name: 'WSL - Ubuntu',
+                        shellPath: 'wsl.exe',
+                        env: {},
+                    });
+
+                    customTerminal = { terminal, cwd: opn };
+                    createdTerminals.push(customTerminal);
+                }
+
+                // Build and run the command
+                const gccCommand = `gcc "${filePath}" -lm -o "${op}" && chmod +x "${op}" && "${op}" && rm "${op}"`;
+
+                // Send the command to the terminal
+                customTerminal.terminal.sendText(gccCommand, true);
+
+                // Show the terminal
+                customTerminal.terminal.show();
+            } else if (filePath.endsWith('.java')) {
+                const className = path.parse(filePath).name;
+                const opn = filePath.replace(className + ".java", "");
+
+                // Check if a terminal for Java files already exists
+                customTerminal = createdTerminals.find(term => term.terminal.name === 'CMD - Windows' && term.cwd === opn);
+
+                if (!customTerminal) {
+                    // Create a new terminal
+                    const terminal = vscode.window.createTerminal({
+                        name: 'CMD - Windows',
+                        shellPath: 'cmd.exe',
+                        env: {},
+                    });
+
+                    customTerminal = { terminal, cwd: opn };
+                    createdTerminals.push(customTerminal);
+                }
+
+                // Build and run the command for Java file in CMD
+                const javaCommand = `cd ${opn} && javac ${className}.java && java ${className} && del ${className}.class`;
+
+                // Send the command to the terminal
+                customTerminal.terminal.sendText(javaCommand, true);
+
+                // Show the terminal
+                customTerminal.terminal.show();
+            } else {
+                vscode.window.showWarningMessage('Unsupported file type.');
+            }
+        } else {
+            vscode.window.showWarningMessage('Open a file to run.');
+        }
+    });
+
+    context.subscriptions.push(disposable);
 }
